5af758a6a7ec061ad9636e20464db09a153b6fbc
FlutLab.io <>
2022-01-03 05:18:02 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData(
-      brightness: Brightness.dark,
+      brightness: Brightness.light,
     ),
     home: SignUpPage(),
   ));
