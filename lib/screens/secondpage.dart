582640f2c05ffbf1a6386ed36808ad5663d2ee51
FlutLab.io <>
2022-01-08 05:37:26 +0000

    Autosave

--- lib/screens/secondpage.dart
+++ lib/screens/secondpage.dart
@@ -2,7 +2,6 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
-
 class SecondPage extends StatefulWidget {
   _SecondPageState createState() => _SecondPageState();
 }
@@ -11,16 +10,14 @@ class _SecondPageState extends State<SecondPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-          appBar: AppBar(
-            title: Text('title'),
-          ),
-          body: Center(
-            child: Text(
-              'Text',
-            ),
-          ),
-        );(
-        
+      appBar: AppBar(
+        title: Text('title'),
+      ),
+      body: Center(
+        child: Text(
+          'Text',
+        ),
+      ),
     );
   }
 }
